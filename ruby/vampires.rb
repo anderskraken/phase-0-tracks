a7b67e017b3e0512ad09b83fc96eec2f2a5885d6
anderskraken <tunold@gmail.com>
2016-06-15 16:17:55 +0200

    Add Vampire questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.to_i
+
+puts "What year were you born?"
+born = gets.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+like_garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+want_insurance = gets.chomp
