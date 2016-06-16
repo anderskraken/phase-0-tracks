f60dfe3b2bdc41ba0781b4fd362520da622ae210
anderskraken <tunold@gmail.com>
2016-06-16 11:29:34 +0200

    Add check for allergies, correct conditional logic for release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -19,7 +19,7 @@ while employees > processed_employees
   elsif born == 2015 - age
     correct_age = true
   else
-    correct_age = false
+    wrong_age = true
   end
 
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
@@ -28,7 +28,7 @@ while employees > processed_employees
   if like_garlic == "y"
     like_garlic = true
   else
-    like_garlic = false
+    hate_garlic = true
   end
 
   puts "Would you like to enroll in the company’s health insurance? (y/n)"
@@ -37,20 +37,59 @@ while employees > processed_employees
   if want_insurance == "y"
     want_insurance = true
   else
-    want_insurance = false
+    no_insurance = true
   end
 
-  if correct_age && like_garlic && want_insurance == true
-    puts "Probably not a vampire."
-  elsif !correct_age && !like_garlic && !want_insurance == true
-    puts "Almost certainly a vampire."
-  elsif !correct_age && (!like_garlic || !want_insurance) == true
-    puts "Probably a vampire."
-  elsif name == ("Drake Cula" || "Tu Fang")
-    puts "Definitely a vampire"
-  else
-    puts "Results inconclusive."
+
+  vampire_probability = "Results inconclusive."
+  
+  if correct_age && like_garlic && want_insurance
+    vampire_probability = "Probably not a vampire."
   end
+
+  if wrong_age && (hate_garlic || no_insurance)
+    vampire_probability = "Probably a vampire."
+  end
+
+  if wrong_age && hate_garlic && no_insurance
+    vampire_probability = "Almost certainly a vampire."
+  end
+
+  if name == ("Drake Cula" || "Tu Fang")
+    vampire_probability = "Definitely a vampire"
+  elsif name != ("Drake Cula" || "Tu Fang")
+  end
+
+
+  puts "Please name any allergies, one at a time. When you have named all your allergies, type 'done'."
+  
+  allergies = ""
+
+  until allergies == "done"
+    allergies = gets.chomp
+    break if allergies == "sunshine"    
+  end
+
+  if allergies == "sunshine" 
+    vampire_probability = "Probably a vampire."
+  end
+
+
+  puts vampire_probability
+
+#  Alternative logic for Release 2 with bang operators - I scrapped this after re-reading the assignment
+#
+#  if correct_age && like_garlic && want_insurance == true
+#    puts "Probably not a vampire."
+#  elsif !correct_age && !like_garlic && !want_insurance == true
+#    puts "Almost certainly a vampire."
+#  elsif !correct_age && (!like_garlic || !want_insurance) == true
+#    puts "Probably a vampire."
+#  elsif name == ("Drake Cula" || "Tu Fang")
+#    puts "Definitely a vampire"
+#  else
+#    puts "Results inconclusive."
+#  end
 
 end
 
