9254ed06e2f62d2172fd2f3f2f12f3547f3bb74c
anderskraken <tunold@gmail.com>
2016-06-16 10:15:38 +0200

    Add loop to process multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,48 +1,56 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed?"
+employees = gets.to_i
+processed_employees = 0
 
-puts "How old are you?"
-age = gets.to_i
+while employees > processed_employees
+  processed_employees +=1 
 
-puts "What year were you born?"
-born = gets.to_i
+  puts "What is your name?"
+  name = gets.chomp
 
-if born == 2016 - age
-  correct_age = true
-elsif born == 2015 - age
-  correct_age = true
-else
-  correct_age = false
+  puts "How old are you?"
+  age = gets.to_i
+
+  puts "What year were you born?"
+  born = gets.to_i
+
+  if born == 2016 - age
+    correct_age = true
+  elsif born == 2015 - age
+    correct_age = true
+  else
+    correct_age = false
+  end
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+  like_garlic = gets.chomp
+
+  if like_garlic == "y"
+    like_garlic = true
+  else
+    like_garlic = false
+  end
+
+  puts "Would you like to enroll in the company’s health insurance? (y/n)"
+  want_insurance = gets.chomp
+
+  if want_insurance == "y"
+    want_insurance = true
+  else
+    want_insurance = false
+  end
+
+  if correct_age && like_garlic && want_insurance == true
+    puts "Probably not a vampire."
+  elsif !correct_age && !like_garlic && !want_insurance == true
+    puts "Almost certainly a vampire."
+  elsif !correct_age && (!like_garlic || !want_insurance) == true
+    puts "Probably a vampire."
+  elsif name == ("Drake Cula" || "Tu Fang")
+    puts "Definitely a vampire"
+  else
+    puts "Results inconclusive."
+  end
+
 end
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-like_garlic = gets.chomp
-
-if like_garlic == "y"
-  like_garlic = true
-else
-  like_garlic = false
-end
-
-puts "Would you like to enroll in the company’s health insurance? (y/n)"
-want_insurance = gets.chomp
-
-if want_insurance == "y"
-  want_insurance = true
-else
-  want_insurance = false
-end
-
-if correct_age && like_garlic && want_insurance == true
-  puts "Probably not a vampire."
-elsif !correct_age && !like_garlic && !want_insurance == true
-  puts "Almost certainly a vampire."
-elsif !correct_age && (!like_garlic || !want_insurance) == true
-  puts "Probably a vampire."
-elsif name == ("Drake Cula" || "Tu Fang")
-  puts "Definitely a vampire"
-else
-  puts "Results inconclusive."
-end
-
 
