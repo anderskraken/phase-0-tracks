170244541eb8e1e110b7e12e622a7cd47aa534c7
anderskraken <tunold@gmail.com>
2016-06-27 15:22:18 +0200

    Add puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# PUPPY CLASS
+# 
+# SPECIES ------------------------
+# Canis lupus familiaris catulus
+# 
+# CHARACTERISTICS ----------------
+# Fur length: varies
+# Friendly: yes
+# Leg count: 4
+# Name: varies
+# Color: varies
+# 
+# BEHAVIOR -----------------------
+# Lick
+# Sleep
+# Bark
+# Chew
+# Dig
